740466c53e6f031d97b72e469516a2c08e9f665a
brettmarquard <brett.marquard1@gmail.com>
2017-05-23 10:13:18 -0400

    removing files associated with FHIR IG publisher

--- pages/_includes/profiles.md
+++
@@ -1,9 +0,0 @@
-# {{ page.title }}
-
-source pages/_include/{{page.md_filename}}.md  file
-
-#### Profiles
-
-The following Profiles have been defined for this implementation guide.
-
-{% include list-simple-profiles.xhtml %}
--- publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-name="Argonaut CDS Hooks"
-path1=/Users/ehaas/Documents/FHIR/CDS-Hooks/
-path2=/Users/ehaas/Downloads/
-echo "================================================================="
-echo === Publish $name IG!!! $(date -u) ===
-echo "================================================================="
-git status
-java -jar ${path2}org.hl7.fhir.igpublisher.jar -ig ${path1}ig.json -watch
